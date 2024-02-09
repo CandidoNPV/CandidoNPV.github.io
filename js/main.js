b1768fbf5f48ab03e2336b1429ee12fa8d37d0a4
Kanosk <candidonpv@gmail.com>
2024-02-09 18:05:35 +0000

    se hacen un par de cambios

--- js/main.js
+++ js/main.js
@@ -26,21 +26,23 @@ const mostrarKnowledge = ()=>{
     knowledge.innerHTML = "aqui hablo sobre mis conocimientos";
 }
 const mostrarContact = ()=>{
-    borrar();
     contact.style.display ="block"
-    contact.innerHTML = "aqui pongo mis datos de contacto";
 }
 document.getElementById('btnSwitch').addEventListener('click', () => {
     const currentTheme = document.documentElement.getAttribute('data-bs-theme');
     const moonIcon = '<i class="fa-solid fa-moon"></i>';
     const sunIcon = '<i class="fa-solid fa-sun"></i>';
+    const lightFile = '<i class="fa-solid fa-file-pdf"></i>';
+    const darkFile = '<i class="fa-regular fa-file-pdf"></i>';
     
     if (currentTheme === 'dark') {
         document.documentElement.setAttribute('data-bs-theme', 'light');
         document.getElementById('btnSwitch').innerHTML = moonIcon;
+        document.getElementById('cv').innerHTML = lightFile;
     } else {
         document.documentElement.setAttribute('data-bs-theme', 'dark');
         document.getElementById('btnSwitch').innerHTML = sunIcon;
+        document.getElementById('cv').innerHTML = darkFile;
     }
 });
 
